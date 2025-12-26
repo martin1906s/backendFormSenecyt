3f53eb71bd5cebb9bba534de4808a077f511076e
Tin <136649867+martin1906s@users.noreply.github.com>
2025-12-26 12:24:31 -0500

    Update CORS configuration in main.ts to restrict origins and enhance security
    - Modified CORS settings to allow specific origins instead of wildcard.
    - Added support for OPTIONS method and included additional allowed headers.
    - Enabled credentials and adjusted preflight response settings.

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: [
+      'https://forms-senecyt.vercel.app',
+      'https://forms-senecyt-git-main-martin1906s.vercel.app',
+      'http://localhost:4200',
+      'http://localhost:3000',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
   app.useGlobalPipes(
     new ValidationPipe({
